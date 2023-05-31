ce238df616ab24fda10d09dbdcb0c2e512ff47d1
Hüseyin Yiğit Ülker <ulkerhuseyin1905@gmail.com>
2023-06-01 00:11:48 +0300

    detection part updated
    comment added

--- Code/Detection/detection_img.py
+++ Code/Detection/detection_img.py
@@ -1,11 +1,27 @@
+"""
+    Performs object detection on images using the YOLO model and may saves the results.
+
+    Args:
+        model (YOLO): The YOLO object detection model.
+        file_path (str): The path to the directory containing the input images.
+        result_path (str): The path to the directory where the detection results will be saved.
+
+"""
+
+
 from ultralytics import YOLO
 import os
 import cv2 as cv
-model = YOLO("models\\baseline_model.pt")
-file_path = "Edited GTSRB Dataset\\test_img"
+
+# Load YOLO model
+model = YOLO("..\Models\\baseline_model.pt")
+
+file_path = "..\Edited GTSRB Dataset\\test_img"
+
 result_path = "Results\\baseline_model"
 
 for i in os.listdir(file_path):
-
-    model.predict(source=file_path+"\\"+i, show=False, save=True)
+    model.predict(source=file_path+"\\"+i, show=True, save=True ,project = result_path)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
--- Code/Detection/detection_video.py
+++ Code/Detection/detection_video.py
@@ -1,54 +1,50 @@
+"""
+   Performs object detection on a video using the YOLO model and displays the results.
+
+   Args:
+       model (YOLO): The YOLO object detection model.
+       video_path (str): The path to the input video.
+"""
+
 from ultralytics import YOLO
 import cv2 as cv
 import cvzone
 import math
 
-model = YOLO("models\\baseline_model.pt")
+# Set the path to the YOLO model and input video
+model_path = "../Results/model_4/best.pt"
+video_path = "../Video_Data/test1.mp4"
+
+model = YOLO(model_path)
 
 #cap = cv.VideoCapture("..\Videos\ppe-2.mp4")
 
-cap = cv.VideoCapture("Video_Data\\video-1.mp4")
+cap = cv.VideoCapture(video_path)
 cap.set(3,640)
 cap.set(4,640)
 
-"""classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]"""
-
-"""classNames = ['Excavator', 'Gloves', 'Hardhat', 'Ladder', 'Mask', 'NO-Hardhat', 'NO-Mask', 'NO-Safety Vest', 'Person', 'SUV', 'Safety Cone', 'Safety Vest', 'bus', 'dump truck', 'fire hydrant', 'machinery', 'mini-van', 'sedan', 'semi', 'trailer', 'truck and trailer', 'truck', 'van', 'vehicle', 'wheel loader']
-"""
-classNames =  ['speed limit 20', 'speed limit 30', 'speed limit 50', 'speed limit 60', 'speed limit 70', 'NO-speed limit 80', 'restriction ends 80', 'speed limit 100', 'speed limit 120',
+"""classNames =  ['speed limit 20', 'speed limit 30', 'speed limit 50', 'speed limit 60', 'speed limit 70', 'NO-speed limit 80', 'restriction ends 80', 'speed limit 100', 'speed limit 120',
 'no overtaking', 'no overtaking (trucks)', 'priority at next intersection', 'priority road', 'give way', 'stop', 'no traffic both ways', 'no trucks', 'no entry', 'danger',
 'bend left', 'bend right', 'bend', 'uneven road', 'slippery road', 'road narrows','construction', 'traffic signal', 'pedestrian crossing', 'school crossing', 'cycles crossing',
 'snow', 'animals', 'restriction ends', 'go right', 'go left', 'go straight', 'go right or straight', 'go left or straight', 'keep right', 'keep left', 'roundabout',
-'restriction ends (overtaking)', 'restriction ends (overtaking (trucks))']
+'restriction ends (overtaking)', 'restriction ends (overtaking (trucks))']"""
+
+classNames =['ANIMALS', 'CONSTRUCTION', 'CYCLES CROSSING', 'DANGER', 'NO ENTRY', 'PEDESTRIAN CROSSING', 'SCHOOL CROSSING', 'SNOW', 'STOP', 'bend left', 'bend right', 'bend', 'give way', 'go left or straight', 'go left', 'go right or straight', 'go right', 'go straight', 'keep left', 'keep right', 'no overtaking (trucks)', 'no overtaking -trucks-', 'no overtaking', 'no traffic both ways', 'no trucks', 'priority at next intersection', 'priority road', 'restriction ends (overtaking (trucks))', 'restriction ends (overtaking)', 'restriction ends -overtaking -trucks--', 'restriction ends -overtaking-', 'restriction ends 80', 'restriction ends', 'road narrows', 'roundabout', 'slippery road', 'speed limit 100', 'speed limit 120', 'speed limit 20', 'speed limit 30', 'speed limit 50', 'speed limit 60', 'speed limit 70', 'speed limit 80', 'traffic signal', 'uneven road']
+
 while True:
     success, img = cap.read()
     result = model(img,stream=True)
     for r in result:
         for box in r.boxes:
             x1,y1,x2,y2 = box.xyxy[0]
-            """x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-            cv.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3,)"""
-
             x,y,w,h = x1,y1,x2-x1,y2-y1
-
             bbox= (int(x),int(y),int(w),int(h))
 
-            cvzone.cornerRect(img,bbox)
-
             conf = math.ceil((box.conf[0]*100))/100
-
             cls = box.cls[0]
-            cvzone.putTextRect(img, f'{conf} {classNames[int(cls)]}', pos = (int(max(0, x1)), int(max(35, y1))), scale=1, thickness=1)
+            if conf > 0.5 :
+                cvzone.cornerRect(img, bbox)
+                cvzone.putTextRect(img, f'{conf} {classNames[int(cls)]}', pos = (int(max(0, x1)), int(max(35, y1))), scale=1, thickness=1)
 
 
     cv.imshow("img",img)
